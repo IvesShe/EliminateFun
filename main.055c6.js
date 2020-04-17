79e47ed434f80aff384a15ae78fe9855bc5e30ab
Ives She <ivesshe@gmail.com>
2020-04-17 19:35:36 +0800

    修正bug

--- main.055c6.js
+++ main.055c6.js
@@ -140,7 +140,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.78deb.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.728c1.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
